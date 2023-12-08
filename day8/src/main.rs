8f9606e14ad9a875ea761423cc8d35e1edef91dd
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2023-12-08 19:54:11 +0700

    day 8 - part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::BTreeMap, fs};
+use std::{
+    collections::{BTreeMap, HashMap},
+    fs,
+};
 
 fn main() {
     let content = fs::read_to_string("input.txt").unwrap();
@@ -15,15 +18,39 @@ fn main() {
         let (current, branch) = extract_path(line);
         maps.insert(current, branch);
     }
-    let mut steps = 0;
-    let mut pos = "AAA";
-    while pos != "ZZZ" {
-        let next = paths[steps % paths.len()];
-        pos = maps[pos][next];
-        println!("{pos}");
+    let mut steps: u64 = 0;
+    // Part 1
+    // let mut pos = "AAA";
+    // while pos != "ZZZ" {
+    //     let next = paths[steps % paths.len()];
+    //     pos = maps[pos][next];
+    //     steps += 1;
+    // }
+    // println!("{steps}");
+    let mut pos_ghost = maps
+        .iter()
+        .map(|(&c, _)| c)
+        .filter(|&c| &c[2..] == "A")
+        .collect::<Vec<_>>();
+    let mut pos_steps = HashMap::new();
+    while pos_steps.len() != pos_ghost.len() {
+        let next = paths[steps as usize % paths.len()];
+        let mut i = 0;
+        for pos in &mut pos_ghost {
+            *pos = maps[pos][next];
+            if &pos[2..] == "Z" {
+                pos_steps.entry(i).or_insert(steps + 1);
+            }
+            i += 1;
+        }
         steps += 1;
     }
-    println!("{steps}");
+    println!("{pos_steps:?}");
+    steps = 0;
+    for (_, step) in pos_steps {
+        steps = if steps == 0 { step } else { lcm(steps, step) };
+    }
+    println!("ghost_step {steps}");
 }
 
 fn extract_path(input: &str) -> (&str, Vec<&str>) {
@@ -33,4 +60,19 @@ fn extract_path(input: &str) -> (&str, Vec<&str>) {
     let right = &inputs[3][0..3];
     let branch = vec![left, right];
     (current, branch)
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    (a / gcd(a,b)) * b
 }
